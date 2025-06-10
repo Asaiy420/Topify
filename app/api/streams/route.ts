06a36ee14011ccea3fc877d32f27d3481c1d3ee2
Asaiy420 <kcaryan05@gmail.com>
2025-06-10 15:46:31 +0545

    add: routes to fetch youtube api for metadata

--- app/api/streams/route.ts
+++ app/api/streams/route.ts
@@ -1,7 +1,7 @@
 import { NextRequest, NextResponse } from "next/server";
 import { z } from "zod";
 import { prismaClient } from "@/app/lib/db";
-
+import * as play from "play-dl";
 const YT_REGEX = new RegExp(
   "^https?://(www\\.)?youtube\\.com/watch\\?v=[\\w-]{11}(&t=\\d+s)?$"
 );
@@ -27,21 +27,41 @@ export async function POST(req: NextRequest) {
 
     const extractedId = data.url.split("v=")[1].split("&")[0];
 
-    const stream = await prismaClient.stream.create({
-      data: {
-        userId: data.creatorId,
-        url: data.url,
-        type: "Youtube",
-        extractedId,
-      },
-    });
+    try {
+      const info = await play.video_basic_info(`https://www.youtube.com/watch?v=${extractedId}`);
+      const res = info.video_details;
+      const thumbnails = res.thumbnails ; 
+      thumbnails.sort((a: {width: number}, b: {width: number}) => a.width < b.width ? -1 : 1);
 
-    return NextResponse.json({
-      message: "Stream added successfully",
-      id: stream.id,
 
-    })
-    } catch (e) {
+      const stream = await prismaClient.stream.create({
+        data: {
+          userId: data.creatorId,
+          url: data.url,
+          extractedId,
+          type: "Youtube",
+          title: res.title ?? "The Stupid API DID NOT WORK!!",
+          smallImg: thumbnails.length > 1  ? thumbnails[thumbnails.length - 2].url : thumbnails[thumbnails.length - 1].url ?? "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRXo8oXCvHl8xblof25yDlIC5SUw7_p2TW-Mw&s",
+          bigImg: thumbnails[thumbnails.length - 1].url ?? "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRXo8oXCvHl8xblof25yDlIC5SUw7_p2TW-Mw&s",
+
+        },
+      });
+
+      return NextResponse.json({
+        message: "Stream added successfully",
+        id: stream.id,
+      });
+    } catch (error) {
+      console.error("Error details:", error);
+      return NextResponse.json(
+        {
+          message: "Error when adding a stream",
+          error: error instanceof Error ? error.message : "Unknown error",
+        },
+        { status: 411 }
+      );
+    }
+  } catch (e) {
     return NextResponse.json(
       {
         message: "Error when adding a stream",
@@ -52,12 +72,13 @@ export async function POST(req: NextRequest) {
   }
 }
 
-export async function GET(req: NextRequest){
+export async function GET(req: NextRequest) {
   const creatorId = req.nextUrl.searchParams.get("creatorId");
   const streams = await prismaClient.stream.findMany({
     where: {
       userId: creatorId ?? "",
-    }
-  })
+    },
+  });
   return NextResponse.json(streams);
 }
+
